752b230e273012585ea8ce647d05f495f6d432d4
Byron Blocker and Marilyn Carlin <marilyn@>
2017-03-06 20:52:12 -0800

    add additional js user interface logic to allow for capital letters in user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // business logic
 function reverseString(string) {
   return string.split("").reverse().join("");
-  alert(string);
 }
-console.log(reverseString);
+// console.log(reverseString);
 var palindromeCheck = function(string, reversedString) {
   if (string === reversedString) {
     return true;
@@ -15,10 +14,10 @@ var palindromeCheck = function(string, reversedString) {
 $(document).ready(function() {
   $("form#word-check").submit(function(event) {
     event.preventDefault();
-    var string = $("input#string").val();
+    var string = $("input#string").val().toLowerCase();
     var reversedString = reverseString(string);
     var result = palindromeCheck(string, reversedString);
-    // $("#result").text(result);
+
 
     $(".string").text(string);
 
